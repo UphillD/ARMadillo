911d282db9a1dcacd38b135cd06d7c6ca12ad0e1
UphillD <13258958+UphillD@users.noreply.github.com>
2020-03-08 20:59:09 +0200

    Organize the project tree, improve Makefile

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,13 +1,13 @@
 extern void PUT32 (unsigned int, unsigned int);
 extern unsigned int GET32 (unsigned int);
-extern void dummy (unsigned int);
+extern void delay (unsigned int);
 
 #define GPFSEL3	0x2020000C
 #define GPFSEL4	0x20200010
 #define GPSET1	0x20200020
 #define GPCLR1	0x2020002C
 
-int kernel_main (void)
+void init_led (void)
 {
 	unsigned int ra;
 
@@ -15,23 +15,30 @@ int kernel_main (void)
 	ra &= ~(7 << 21);
 	ra |= (1 << 21);
 	PUT32(GPFSEL4, ra);
+}
+
+int kernel_main (void)
+{
+	unsigned int ra;
+
+	init_led();
 
 	while (1) {
 		PUT32(GPCLR1, 1 << (47 - 32));
 		for (ra = 0; ra < 0x80000; ra++) {
-			dummy(ra);
+			delay(ra);
 		}
 		PUT32(GPSET1, 1 << (47 - 32));
 		for (ra = 0; ra < 0x80000; ra++) {
-			dummy(ra);
+			delay(ra);
 		}
 		PUT32(GPCLR1, 1 << (47 - 32));
 		for (ra = 0; ra < 0x80000; ra++) {
-			dummy(ra);
+			delay(ra);
 		}
 		PUT32(GPSET1, 1 << (47 - 32));
 		for (ra = 0; ra < 0x300000; ra++) {
-			dummy(ra);
+			delay(ra);
 		}
 	}
 	return 0;
