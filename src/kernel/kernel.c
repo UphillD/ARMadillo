6a87bdf682f3681d20c1965fc10a9550895624d1
UphillD <13258958+UphillD@users.noreply.github.com>
2020-03-08 22:25:03 +0200

    Implement sleep function

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,10 +2,27 @@ extern void PUT32 (unsigned int, unsigned int);
 extern unsigned int GET32 (unsigned int);
 extern void delay (unsigned int);
 
+#define SYSTIMER 0x20003004
 #define GPFSEL3	0x2020000C
 #define GPFSEL4	0x20200010
 #define GPSET1	0x20200020
 #define GPCLR1	0x2020002C
+
+void sleep (unsigned int msec)
+{
+	unsigned int sleepTime = msec * 977;
+	unsigned int currTime;
+
+	while (1) {
+		currTime = GET32(SYSTIMER);
+		if ((currTime &= sleepTime) == sleepTime) break;
+	}
+	while (1) {
+		currTime = GET32(SYSTIMER);
+		if ((currTime &= sleepTime) == 0) break;
+	}
+	return;
+}
 
 void init_led (void)
 {
@@ -19,27 +36,18 @@ void init_led (void)
 
 int kernel_main (void)
 {
-	unsigned int ra;
 
 	init_led();
 
 	while (1) {
 		PUT32(GPCLR1, 1 << (47 - 32));
-		for (ra = 0; ra < 0x80000; ra++) {
-			delay(ra);
-		}
+		sleep(120);
 		PUT32(GPSET1, 1 << (47 - 32));
-		for (ra = 0; ra < 0x80000; ra++) {
-			delay(ra);
-		}
+		sleep(120);
 		PUT32(GPCLR1, 1 << (47 - 32));
-		for (ra = 0; ra < 0x80000; ra++) {
-			delay(ra);
-		}
+		sleep(120);
 		PUT32(GPSET1, 1 << (47 - 32));
-		for (ra = 0; ra < 0x300000; ra++) {
-			delay(ra);
-		}
+		sleep(1200);
 	}
 	return 0;
 }
