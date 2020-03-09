7527d8ddbe2c6421203abec28313f8575515b610
UphillD <13258958+UphillD@users.noreply.github.com>
2020-03-09 23:33:43 +0200

    Update the SD Card loader script

--- build/sdloader.sh
+++ build/sdloader.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+
+# Create directory /mount/raspberry
+mkdir -p /mount/raspberry
+
+# Mount the boot partition of the SD Card to /mount/raspberry
 mount /dev/mmcblk0p1 /mount/raspberry
+
+# Copy the kernel image to the SD Card
 cp kernel.img /mount/raspberry/kernel.img
+
+# Make sure that the file is written to the SD Card and not in some buffer
 sync
+
+# Unmount the SD Card
 umount /dev/mmcblk0p1
+
+# Easy way to check if the SD Card is safe to remove
 lsblk -p
